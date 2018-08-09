1ec6ca18e8d63392476b01ff0604753c52d5d929
willi54901 <13705168271@139.com>
2018-08-09 22:08:56 +0800

    rrr

--- bases-http/src/main/java/com/yaao/bases/http/HttpApp.java
+++ bases-http/src/main/java/com/yaao/bases/http/HttpApp.java
@@ -4,6 +4,7 @@ public class HttpApp {
 	public int i =0;
 	public static void main(String[] args) {
 		System.out.println("HttpApp is run.......................");
+		System.out.println("HttpApp is run.......................");
 
 	}
 
